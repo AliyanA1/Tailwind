d8c41a12af4e5c206e83551a24b3b331be9d4b5e
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-23 15:08:16 +0500

    card jsx

---
+++ src/Components/TailwindDay1/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-64 h-18 m-3 
+     p-2 text-white bg-blue-400 
+     shadow-md rounded-lg  hover:bg-blue-500'>
+        <h2 className='font-mono text-2xl text-center'>Practice tailwind</h2>
+    </div>
+  )
+}
+
+export default Card
