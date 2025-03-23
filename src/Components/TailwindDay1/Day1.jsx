102b8d072469f9432f27c9d622bf6e5f0d212c98
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-23 15:08:05 +0500

    Day1 jsx

---
+++ src/Components/TailwindDay1/Day1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+import Button from './components/Button'
+
+const Day1 = () => {
+  return (
+    <div className='h-screen flex items-center justify-center bg-gray-900'>
+        {/*card component */}
+        <Card/>
+
+        {/* button componet */}
+        <Button/>
+    </div>
+  )
+}
+
+export default Day1
